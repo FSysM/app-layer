1ba2df9e8c4a5180be2678d11a9551de77a7616a
lukkasm <lukkas.massek@gmail.com>
2026-05-09 14:46:07 +0200

    feat: db connect, seed db

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,77 +1,50 @@
-import { PrismaClient, Prisma } from './generated/client';
-import { PrismaPg } from '@prisma/adapter-pg';
+import "dotenv/config";
+import { Pool } from "pg";
+import bcrypt from 'bcrypt'
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from './generated/client'
 
-const pool = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
-const prisma = new PrismaClient({ adapter: pool });
+const connectionString = `${process.env.DATABASE_URL}`;
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
 
-const userData: Prisma.UserCreateInput[] = [
-  {
-    name: 'Alice',
-    email: 'alice@prisma.io',
-    posts: {
-      create: [
-        {
-          title: 'Join the Prisma Discord',
-          content: 'https://pris.ly/discord',
-          published: true,
-        },
-      ],
-    },
-  },
-  {
-    name: 'Nilu',
-    email: 'nilu@prisma.io',
-    posts: {
-      create: [
-        {
-          title: 'Follow Prisma on Twitter',
-          content: 'https://www.twitter.com/prisma',
-          published: true,
-        },
-      ],
-    },
-  },
-  {
-    name: 'Mahmoud',
-    email: 'mahmoud@prisma.io',
-    posts: {
-      create: [
-        {
-          title: 'Ask a question about Prisma on GitHub',
-          content: 'https://www.github.com/prisma/prisma/discussions',
-          published: true,
-        },
-        {
-          title: 'Prisma on YouTube',
-          content: 'https://pris.ly/youtube',
-        },
-      ],
-    },
-  },
-];
 
 async function main() {
-  console.log(`Start seeding ...`);
+  const hashedPassword = await bcrypt.hash('password', 10)
 
-  // Clear existing data
-  await prisma.post.deleteMany();
-  await prisma.user.deleteMany();
+  console.log('Seeding...')
 
-  for (const u of userData) {
-    const user = await prisma.user.create({
-      data: u,
-    });
-    console.log(`Created user with id: ${user.id}`);
-  }
-  console.log(`Seeding finished.`);
+  await prisma.user.createMany({
+    data: [
+      {
+        username: 'student1',
+        email: 'student1@email.com',
+        password: hashedPassword,
+        role: 'STUDENT',
+        name: 'Student One',
+        phone: '123456789',
+        address: 'Prague'
+      },
+      {
+        username: 'teacher1',
+        email: 'teacher1@email.com',
+        password: hashedPassword,
+        role: 'TEACHER',
+        name: 'Teacher One',
+        phone: '123456789',
+        address: 'Prague'
+      }
+    ]
+  })
+
+  console.log('Done')
 }
 
 main()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
+  .then(() => prisma.$disconnect())
   .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
