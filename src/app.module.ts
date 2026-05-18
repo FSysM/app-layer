84376cbc9bfc467fb48fc956de97f8fcc0a738ec
lukkasm <lukkas.massek@gmail.com>
2026-05-18 19:06:41 +0200

    feat: health check

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
 import { PrismaModule } from './prisma/prisma.module';
+import { HealthController } from './health/health.controller';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { AssignmentsModule } from './assignments/assignments.module';
@@ -18,5 +19,6 @@ import { ReviewsModule } from './reviews/reviews.module';
     SubmissionsModule,
     ReviewsModule,
   ],
+  controllers: [HealthController],
 })
 export class AppModule {}
