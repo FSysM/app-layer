851c24c5502590fd2a52dd16b170dd221ede1172
lukkasm <lukkas.massek@gmail.com>
2026-05-18 18:59:59 +0200

    chore: better guards in auth module

--- src/auth/guards/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth.guard.ts
@@ -1,19 +1,14 @@
-import { ExecutionContext, Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
-  constructor(private options?: { optional?: boolean }) {
-    super();
-  }
-
   canActivate(context: ExecutionContext) {
     return super.canActivate(context);
   }
 
   handleRequest(err: any, user: any) {
-    if (this.options?.optional) return user ?? undefined;
-    if (err || !user) throw err;
+    if (err || !user) throw err ?? new UnauthorizedException();
     return user;
   }
 }
